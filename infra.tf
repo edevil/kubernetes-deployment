62078712a0524ab5b05fad5238754eb56f0a147d
André Cruz <andre@brpx.com>
2016-10-25 22:44:28 +0100

    Added security rule for SSH.

--- infra.tf
+++ infra.tf
@@ -294,6 +294,19 @@ resource "azurerm_network_security_group" "master-sg" {
         source_address_prefix = "*"
         destination_address_prefix = "*"
     }
+
+    security_rule {
+        name = "allow_ssh"
+        priority = 110
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+
 }
 
 resource "azurerm_public_ip" "masterPUBIP" {
