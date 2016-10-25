1515efdf064c48b7e5655d094b562fc4c588015a
André Cruz <andre@brpx.com>
2016-10-25 18:48:25 +0100

    Restore jumpbox. Ansible still needs it to reach the machines.

--- infra.tf
+++ infra.tf
@@ -124,6 +124,14 @@ resource "azurerm_subnet" "node" {
     network_security_group_id = "${azurerm_network_security_group.kubesg.id}"
 }
 
+resource "azurerm_subnet" "management" {
+    name = "management"
+    address_prefix = "10.0.254.0/24"
+    resource_group_name = "${azurerm_resource_group.kuberg.name}"
+    virtual_network_name = "${azurerm_virtual_network.network.name}"
+    route_table_id = "${azurerm_route_table.kubetable.id}"
+}
+
 resource "azurerm_storage_account" "disks_account" {
   name = "${lower(var.resource_group)}disks"
   resource_group_name = "${azurerm_resource_group.kuberg.name}"
@@ -136,6 +144,65 @@ resource "azurerm_storage_container" "disks_container" {
     resource_group_name = "${azurerm_resource_group.kuberg.name}"
     storage_account_name = "${azurerm_storage_account.disks_account.name}"
     container_access_type = "private"
+}
+
+resource "azurerm_public_ip" "jboxPUBIP" {
+    name = "jumpboxPublicIP"
+    location = "${var.region}"
+    resource_group_name = "${azurerm_resource_group.kuberg.name}"
+    public_ip_address_allocation = "dynamic"
+    domain_name_label = "${var.resource_group}-jbox"
+}
+
+resource "azurerm_network_interface" "jboxNIC" {
+    name = "jumpboxnic"
+    location = "${var.region}"
+    resource_group_name = "${azurerm_resource_group.kuberg.name}"
+
+    ip_configuration {
+        name = "jboxipconfiguration"
+        subnet_id = "${azurerm_subnet.management.id}"
+        private_ip_address_allocation = "dynamic"
+        public_ip_address_id = "${azurerm_public_ip.jboxPUBIP.id}"
+    }
+}
+
+resource "azurerm_virtual_machine" "jumpbox" {
+    name = "jumpbox"
+    location = "${var.region}"
+    resource_group_name = "${azurerm_resource_group.kuberg.name}"
+    network_interface_ids = ["${azurerm_network_interface.jboxNIC.id}"]
+    vm_size = "Standard_A0"
+    delete_data_disks_on_termination = "true"
+    delete_os_disk_on_termination = "true"
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "16.04.0-LTS"
+        version = "latest"
+    }
+
+    storage_os_disk {
+        name = "jumpboxdisk"
+        vhd_uri = "${azurerm_storage_account.disks_account.primary_blob_endpoint}${azurerm_storage_container.disks_container.name}/${azurerm_resource_group.kuberg.name}-jumpbox.vhd"
+        caching = "ReadWrite"
+        create_option = "FromImage"
+    }
+
+    os_profile {
+        computer_name = "jumpbox"
+        admin_username = "${var.username}"
+        admin_password = "Password1234!"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path = "/home/${var.username}/.ssh/authorized_keys"
+            key_data = "${file("${var.ssh_key_location}")}"
+        }
+    }
 }
 
 resource "azurerm_availability_set" "etcdAS" {
@@ -275,11 +342,11 @@ resource "azurerm_lb_rule" "httpsLBrule" {
   probe_id = "${azurerm_lb_probe.httpsProbe.id}"
 }
 
-resource "azurerm_lb_nat_rule" "sshNATRule" {
+resource "azurerm_lb_nat_rule" "sshMasterNATRule" {
   location = "${var.region}"
   resource_group_name = "${azurerm_resource_group.kuberg.name}"
   loadbalancer_id = "${azurerm_lb.masterLB.id}"
-  name = "ssh-${count.index}-rule"
+  name = "ssh-master-${count.index}-rule"
   protocol = "Tcp"
   frontend_port = "${30000 + count.index}"
   backend_port = 22
@@ -300,7 +367,7 @@ resource "azurerm_network_interface" "masterNIC" {
         private_ip_address_allocation = "static"
         private_ip_address = "${cidrhost(azurerm_subnet.master.address_prefix, count.index + 10)}"
         load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.masterLBapool.id}"]
-        load_balancer_inbound_nat_rules_ids = ["${element(azurerm_lb_nat_rule.sshNATRule.*.id, count.index)}"]
+        load_balancer_inbound_nat_rules_ids = ["${element(azurerm_lb_nat_rule.sshMasterNATRule.*.id, count.index)}"]
     }
     count = "${var.num_masters}"
 }
